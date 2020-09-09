e48c9c17e44f59d15136c4e338c9c8582cf54b4b
miyuki24 <miyuki.0424@yahoo.ne.jp>
2020-09-09 21:16:41 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 田中美幸 on 2020/09/09.
+//  Copyright © 2020 miyuki.tanaka2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
