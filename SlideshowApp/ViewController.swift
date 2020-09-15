9c07df7db0e41e8c6b3981b5ac76e04b16cb3739
miyuki24 <miyuki.0424@yahoo.ne.jp>
2020-09-15 22:18:29 +0900

    進むボタン、戻るボタンを付けた。スライドのコードの仕組みを理解するために1行1行コメントを追加した。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     //画像を表示する変数
     @IBOutlet weak var DefaultImageView: UIImageView!
+    
+    //ラベルを表示する変数
     @IBOutlet weak var seasonLabel: UILabel!
     
     //スライドショーに使うタイマー
@@ -20,15 +22,16 @@ class ViewController: UIViewController {
     //スライド用の時間のための変数
     var slide_sec: Int = 0
     
-    //画像の登録
+    //画像の宣言
     var seasonImageNames:[UIImage] = [UIImage(named: "spring")!,UIImage(named: "summer")!,UIImage(named: "fall")!,UIImage(named: "winter")!]
     
-    //ラベルの登録
+    //ラベルの宣言
     var seasonText: [String] = ["春","夏","秋","冬"]
     
+    //アプリ起動時に一度だけ機能するコード
     override func viewDidLoad() {
         super.viewDidLoad()
-        //アプリ起動時に一度だけ機能するコード
+        
         //spring画像を表示
         DefaultImageView.image = seasonImageNames[0]
         
@@ -38,39 +41,109 @@ class ViewController: UIViewController {
     
     //進むボタンを機能させる変数
     @IBAction func moveToButton(_ sender: Any) {
+        
+        //表示する画像を一枚進める
+        slide_sec += 1
+        
+        //もし表示する画像と枚数が同じになったら
+        if (slide_sec == seasonImageNames.count){
+            
+            //slide_secを一番初めの画像に戻す
+            slide_sec = 0
+        }
+        
+        //画像を表示する
+        DefaultImageView.image = seasonImageNames[slide_sec]
+        
+        //もし表示するテキストと個数が同じになったら
+        if (slide_sec == seasonText.count){
+            
+            //slide_secを一番初めのテキストに戻す
+            slide_sec = 0
+        }
+        
+        //テキストを表示する
+        seasonLabel.text = seasonText[slide_sec]
     }
     
     //戻るボタンを機能させる変数
     @IBAction func backButton(_ sender: Any) {
+        
+        //表示する画像を一枚戻らせる
+        slide_sec -= 1
+        
+        //もし表示する画像と枚数が同じになったら
+        if (slide_sec == seasonImageNames.count){
+            
+            //slide_secを一番初めの画像の戻す
+            slide_sec = 0
+        }
+        
+        //もしslide_secがマイナスになったら
+        if (slide_sec == -1){
+            
+            //画像を冬に切り替える
+            slide_sec = 3
+        }
+        
+        //画像を表示
+        DefaultImageView.image = seasonImageNames[slide_sec]
+        
+        //もし表示するテキストと個数が同じになったら
+        if (slide_sec == seasonText.count){
+            
+            //slide_secを一番初めのテキストに戻す
+            slide_sec = 0
+        }
+        
+        //テキストを表示
+        seasonLabel.text = seasonText[slide_sec]
     }
     
     //再生・停止ボタンを機能させる変数
     @IBAction func stopButton(_ sender: Any) {
-        // 再生ボタンを押すとスライド作成、始動
+        
+        //もしタイマーが再生中なら
         if (timer == nil) {
+            
+            //タイマーをセットする
             timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
         } else {
+            
+            //タイマーを停止する
             timer.invalidate()
+            
+            //タイマーを停止中にする（nilを使う事で再生中・停止中の判定を行なっている）
             timer = nil
         }
     }
     
     //selector: #selector(updateSlide(_:)) で指定された関数。2秒毎に呼び出される
     @objc func updateTimer(_ slide: Timer) {
+        
+        //画像を一枚進める
         slide_sec += 1
         
+        //もし表示する画像と枚数が同じになったら
         if (slide_sec == seasonImageNames.count){
+            
+            //slide_secを一番初めの画像の戻す
             slide_sec = 0
         }
         
+        //画像を表示
         DefaultImageView.image = seasonImageNames[slide_sec]
         
+        //もし表示するテキストと個数が同じになったら
         if (slide_sec == seasonText.count){
+            
+            //slide_secを一番初めの画像に戻す
             slide_sec = 0
         }
         
+        //テキストを表示
         seasonLabel.text = seasonText[slide_sec]
     }
-    
-    //明日のためにメモ：print関数を使った事で下記のデバックエリアにのみ表示されてしまい、肝心のシュミレーターには反映されていない。尚「進む」ボタンを押せば押すほどデバックに反映される
 }
+
+//明日のためにメモ：print関数を使った事で下記のデバックエリアにのみ表示されてしまい、肝心のシュミレーターには反映されていない。尚「進む」ボタンを押せば押すほどデバックに反映される
